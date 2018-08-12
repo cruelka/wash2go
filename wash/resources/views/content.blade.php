1eb2a3d5da34610a8b0ee7c81914ce2897535455
cruelka <kes1993@ya.ru>
2018-08-13 00:08:50 +0400

    auth added

--- wash/resources/views/content.blade.php
+++ wash/resources/views/content.blade.php
@@ -1,15 +1,30 @@
+
 @extends('layouts.app')
 
 @section('content')
 
+    <div class="news container">
+        <div class="news__item">
+            <div class="row">
+                <div class="col-lg-2">
+                    {{$data->created_at}}
+                </div>
+                <div class="col-lg-10">
+                    <div class="news__item-inner">
+                        <img class="news__item-img" src="{{$data->image}}" alt="Image">
 
-    <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-           {{$data}}
+                        <div class="news-card__title">
+                         {$data->title}}
+                        </div>
 
+                        <div class="news-item__body">
+                            {{$data->body}}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    </div>
 
 @endsection
