c62354ba048a325e8a559a034c7999fef206b88e
cruelka <kes1993@ya.ru>
2018-08-13 00:37:47 +0400

    auth added

--- wash/resources/views/posts.blade.php
+++ wash/resources/views/posts.blade.php
@@ -1,24 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Homepage</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
 
 <div class="container">
-    <br><br>
     <div class="row">
         @foreach($posts as $post)
-            <div class="col-md-3">
-                <a href="/post/{{ $post->slug }}">
-                    <img src="{{ Voyager::image( $post->image ) }}" style="width:100%">
-                    <span>{{ $post->title }}</span>
-                </a>
+
+            <div class="news container">
+                <div class="news__item">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            {{ \Carbon\Carbon::parse($post->created_at)->format('d/m/Y')}}
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="news__item-inner">
+                                <img class="news__item-img" src="/storage/{{$post->image}}" alt="Image">
+
+                                <div class="news-card__lable">
+                                    {{ $post->meta_keywords }}
+                                </div>
+                                <a href="/post/{{ $post->slug }}" class="news-card__title">
+                                    {{ $post->title }}
+                                </a>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
         @endforeach
     </div>
 </div>
 
-</body>
-</html>
+@endsection
