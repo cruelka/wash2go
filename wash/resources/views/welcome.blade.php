196f7513cbdd4dcfa8aa064a7560cc727d8bfd48
socaseinpoint <socaseinpoint@gmail.com>
2018-08-13 18:16:45 +0400

    js

--- wash/resources/views/welcome.blade.php
+++ wash/resources/views/welcome.blade.php
@@ -1182,6 +1182,22 @@
 
           
         });
+
+
+        var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+        var mapOptions = {
+          zoom: 4,
+          center: myLatlng
+        }
+        var map = new google.maps.Map(document.getElementById("map2"), mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            title:"Hello World!"
+        });
+
+        // To add the marker to the map, call setMap();
+        marker.setMap(map);
       }
 
       
