90909590dfbb861fff402931b4561441b726aa2a
socaseinpoint <socaseinpoint@gmail.com>
2018-08-16 17:30:00 +0400

    js

--- wash/resources/views/booking.blade.php
+++ wash/resources/views/booking.blade.php
@@ -362,13 +362,18 @@
 
               <form>
                   <div class="form-group">
+                    <label for="exampleInputEmail1">Name</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                  </div>
+                  <div class="form-group">
                     <label for="exampleInputEmail1">Email</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     <small id="emailHelp" class="form-text text-muted"></small>
                   </div>
                   <div class="form-group">
                     <label for="exampleInputPassword1">Phone</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone">
+                    <input type="phone" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone">
                   </div>
                   <div class="btn btn-success" onclick="">Confirm</div>
                 </form>
