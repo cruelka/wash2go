44a9fda3843522062ed2ae33d6fe771cf589ae5b
socaseinpoint <socaseinpoint@gmail.com>
2018-08-26 23:41:29 +0400

    js

--- wash/resources/views/home.blade.php
+++ wash/resources/views/home.blade.php
@@ -41,12 +41,6 @@
           <p class="category-heading__title">
                 My Bookings
             </p>
-            <p class="category-heading__body">
-            The entire line of Wash2Go® products rely on eco-friendly cleaning, polishing and protecting agents that equal or surpass their chemical-based competitors. Being values-driven people, we are committed to bringing car owners and car wash service providers a superior line of car care products that do not harm the planet or the user.
-            </p>
-
-            
-
       </div>
     </div>
     
