86a316b5275386e8426cce65d4f529444b40fcec
socaseinpoint <socaseinpoint@gmail.com>
2018-08-26 22:15:00 +0400

    js

--- wash/public/js/app.js
+++ wash/public/js/app.js
@@ -52489,7 +52489,7 @@ module.exports = function spread(callback) {
 /* 167 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\n}\r\n^\r\n      Invalid CSS after \"}\": expected 1 selector or at-rule, was \"{\"\r\n      in D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\resources\\assets\\sass\\components\\_header.scss (line 152, column 2)\n    at runLoaders (D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\neo-async\\async.js:7974:18)\n    at options.error (D:\\PRJCT\\Hub\\wash2go\\git\\wash2go\\wash\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
