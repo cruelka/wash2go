3052099fd7ed5aa4792cf6213ef1496857177e0d
cruelka <kes1993@ya.ru>
2018-08-27 02:32:37 +0400

    auth added

--- wash/database/migrations/2018_08_26_215716_create_booking_table.php
+++ wash/database/migrations/2018_08_26_215716_create_booking_table.php
@@ -15,6 +15,16 @@ class CreateBookingTable extends Migration
     {
         Schema::create('booking', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 30)->unique();
+            $table->integer('user_id')->unsigned();
+            $table->string('service');
+            $table->string('price');
+            $table->string('location');
+            $table->string('date');
+            $table->string('time');
+            $table->string('status');
+            $table->string('marks');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
