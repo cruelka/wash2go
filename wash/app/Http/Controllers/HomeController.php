ef1f9c6e71e019d6d292a46f5eb079a1a245b466
cruelka <kes1993@ya.ru>
2018-08-27 06:32:46 +0400

    auth added

--- wash/app/Http/Controllers/HomeController.php
+++ wash/app/Http/Controllers/HomeController.php
@@ -30,7 +30,6 @@ class HomeController extends Controller
         return view('home',compact( 'bookings'));
     }
     public function store(Request $request){
-       dd($request);
         if( Auth::check()) {
             $booking = [
                 'user_id' => Auth::id(),
