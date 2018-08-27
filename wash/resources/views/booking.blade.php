c0be82525bee7749caf5358bd8a00ab8e2be7882
socaseinpoint <socaseinpoint@gmail.com>
2018-08-27 07:46:50 +0400

    js

--- wash/resources/views/booking.blade.php
+++ wash/resources/views/booking.blade.php
@@ -65,7 +65,7 @@
     </div>
 -->
 <p>Click on the map to move marker</p>
-
+<div class="btn" onclick="window.getLocation()">Get current location</div>
     <div id="map"></div>
 
                 </div>
@@ -413,25 +413,33 @@
 
         infoWindow = new google.maps.InfoWindow;
 
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
+        function getLocation(){
+          // Try HTML5 geolocation.
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
 
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    infoWindow.open(map);
-    map.setCenter(pos);
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-}
+              infoWindow.setPosition(pos);
+              infoWindow.setContent('Location found.');
+              infoWindow.open(map);
+              map.setCenter(pos);
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            });
+
+            
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
+
+        }
+
+        window.getLocation = getLocation;
+
 
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
