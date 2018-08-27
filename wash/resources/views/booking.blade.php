644ca8240f68c2da841fb6586d09f1cdeaf1ac6e
socaseinpoint <socaseinpoint@gmail.com>
2018-08-27 07:01:51 +0400

    js

--- wash/resources/views/booking.blade.php
+++ wash/resources/views/booking.blade.php
@@ -421,7 +421,13 @@
       function addMarker(location, map) {
         // Add the marker at the clicked location, and add the next-available label
         // from the array of alphabetical characters.
-        map.removeMarkers();
+        function setMapOnAll(map) {
+          for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+          }
+        }
+
+         setMapOnAll(null);
 
         var marker = new google.maps.Marker({
           position: location,
