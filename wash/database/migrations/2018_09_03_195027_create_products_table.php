7f1149eaca578cf129cad331d0fd952f5a14f486
cruelka <kes1993@ya.ru>
2018-09-03 19:54:26 +0400

    auth added

---
+++ wash/database/migrations/2018_09_03_195027_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30)->unique();
+            $table->integer('category')->unsigned();
+            $table->string('title');
+            $table->string('price');
+            $table->string('description');
+            $table->string('image');
+            $table->foreign('category')->references('id')->on('cproducts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
