d11482a244fb422824894f3bb8777d4b9981b13c
cruelka <kes1993@ya.ru>
2018-09-03 21:12:05 +0400

    auth added

--- wash/resources/views/categories.blade.php
+++ wash/resources/views/categories.blade.php
@@ -49,7 +49,7 @@
     <div class="row">
       <div class="col-lg-4">
           <p class="category-heading__title">
-                Our Products
+              {{$data->title}}
             </p>
             <p class="category-heading__body">
             The entire line of Wash2Go® products rely on eco-friendly cleaning, polishing and protecting agents that equal or surpass their chemical-based competitors. Being values-driven people, we are committed to bringing car owners and car wash service providers a superior line of car care products that do not harm the planet or the user.
