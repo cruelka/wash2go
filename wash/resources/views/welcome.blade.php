fd61039286afd5ef8a2c6179c629aece1e2d3ad6
socaseinpoint <socaseinpoint@gmail.com>
2018-09-03 23:21:58 +0400

    js

--- wash/resources/views/welcome.blade.php
+++ wash/resources/views/welcome.blade.php
@@ -715,8 +715,7 @@
                 <div class="review">
                   <div class="review__heading">What Customers Say</div>
                   <div class="review__inner">
-                    <div class="row">
-                      <div class="col-lg-6 col-md-6">
+
                           <div class="review__item">
                               <img src="{{ asset('images/icons/quotes_green.svg') }}" class="review__item-quotes" alt="Quotes">
                               <div class="review__item-body">
@@ -727,9 +726,7 @@
                                   Al Saeedi
                               </div>
                             </div>
-                      </div>
-
-                      <div class="col-lg-6 col-md-6">
+                  
                           <div class="review__item">
                               <img src="{{ asset('images/icons/quotes_green.svg') }}" class="review__item-quotes" alt="Quotes">
                               <div class="review__item-body">
@@ -1284,6 +1281,23 @@
         },
         768: {
           items: 1
+        }
+        
+    }
+
+    $('.review__inner').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:false,
+      autoplay:false,
+      autoplayTimeout:5000,
+      smartSpeed:450,
+      responsive:{
+        0:{
+            items:1
+        },
+        768: {
+          items: 2
         }
         
     }
