9553e1ebf9ea774c411a706ee6798cab355727f6
cruelka <kes1993@ya.ru>
2018-09-03 20:51:39 +0400

    auth added

--- wash/resources/views/categories.blade.php
+++ wash/resources/views/categories.blade.php
@@ -61,7 +61,7 @@
   
   <div class="sector">
   <div class="row">
-     {{ dd($data)}}
+     {{ dd($data->product )}}
     @foreach($data->product as $product)
   <div class="col-lg-4 col-md-4">
       <div class="product-card product-card_square">
