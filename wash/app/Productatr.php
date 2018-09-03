9c5484b490680c99b09c64ff722f71dd75879b8f
cruelka <kes1993@ya.ru>
2018-09-03 22:22:07 +0400

    auth added

--- wash/app/Productatr.php
+++ wash/app/Productatr.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Attrs;
+use App\Product;
 
 class Productatr extends Model
 {
-    //
+    public function attr()
+    {
+        return $this->HasOne('App\Attrs');
+
+    }
+    public function product()
+    {
+        return $this->HasOne('App\Product');
+
+    }
 }
