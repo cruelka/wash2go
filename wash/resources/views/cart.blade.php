6dfc3d43100eef51742758d7827d516403ffb799
socaseinpoint <socaseinpoint@gmail.com>
2018-09-09 20:18:48 +0400

    js

--- wash/resources/views/cart.blade.php
+++ wash/resources/views/cart.blade.php
@@ -85,7 +85,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> Shipping
+                                        <input type="checkbox" name="remember"> Shipping 10 AED
                                     </label>
                                 </div>
                             </div>
