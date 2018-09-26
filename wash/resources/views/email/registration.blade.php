aab795683e6a582087a3f8267c2b6892bc8a551b
socaseinpoint <socaseinpoint@gmail.com>
2018-09-26 22:37:23 +0400

    js

--- wash/resources/views/email/registration.blade.php
+++ wash/resources/views/email/registration.blade.php
@@ -6,20 +6,20 @@
 </head>
 <body style="background-color:#1BB7CD">
 <div style="max-width: 500px;margin: 0 auto;padding: 20px;background-color:white;">
-<img src="https://wash2go.ae/images/logo.svg" alt="Logo">
-<h1>YOU ARE READY TO GO!!!</h1>
-<h2>Welcome to the Wash2Go Community!</h2>
-<p>
+<img src="https://wash2go.ae/images/logo.png" alt="Logo" style="width: 100px;margin-bottom:20px;">
+<h1 style="color: #3E4C5B">YOU ARE READY TO GO!!!</h1>
+<h2 style="color: #3E4C5B">Welcome to the <a href="https://wash2go.ae/" style="color: #52B947;">Wash2Go</a> Community!</h2>
+<p style="color: #9AA8B6;">
 Hello, {{$user->name}}<br><br>
 We’re excited that you’re here and just moments away from contribution to make your environment healthy for you & upcoming generations.
-<br>
+<br><br>
 YES! It’s a reality.
-<br>
-Wash2Go is a global platform where we save water and environment anywhere
-<br>
-by washing cars without water!
-<br>
-Verify your email address to show our verified email badge on your profile. You can also edit your account settings, connect with Facebook, and add pictures and information to share with our community. Adding images and completing your profile improves trust and transparency of your profile, and makes your profile more credible to other users.
+<br><br>
+Wash2Go is a global platform where we save water and environment anywhere by washing cars without water!
+<br><br>
+Verify your email address to show our verified email badge on your profile.
+<br><br>
+You can also edit your account settings, connect with Facebook, and add pictures and information to share with our community. Adding images and completing your profile improves trust and transparency of your profile, and makes your profile more credible to other users.
 </p>
 </div>
 
