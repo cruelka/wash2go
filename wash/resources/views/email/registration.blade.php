37762838a1ddeca635ad3dd95df710337f6deba0
socaseinpoint <socaseinpoint@gmail.com>
2018-09-26 22:27:55 +0400

    js

--- wash/resources/views/email/registration.blade.php
+++ wash/resources/views/email/registration.blade.php
@@ -4,10 +4,24 @@
 
 
 </head>
-<body>
-<h1 class="introduction__heading-small"> {{$user->name}}, thank you for registering on Wash2go.ae</h1>
-
-
+<body style="background-color:#1BB7CD">
+<div style="max-width: 500px;margin: 0 auto;padding: 20px;background-color:white;">
+<img src="https://wash2go.ae/images/logo.svg" alt="Logo">
+<h1>YOU ARE READY TO GO!!!</h1>
+<h2>Welcome to the Wash2Go Community!</h2>
+<p>
+Hello, {{$user->name}}<br><br>
+We’re excited that you’re here and just moments away from contribution to make your environment healthy for you & upcoming generations.
+<br>
+YES! It’s a reality.
+<br>
+Wash2Go is a global platform where we save water and environment anywhere
+<br>
+by washing cars without water!
+<br>
+Verify your email address to show our verified email badge on your profile. You can also edit your account settings, connect with Facebook, and add pictures and information to share with our community. Adding images and completing your profile improves trust and transparency of your profile, and makes your profile more credible to other users.
+</p>
+</div>
 
 </body>
 
